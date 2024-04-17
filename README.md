d56a12d43bddf6a60d107e5a5afa636635b47dd6
Dan Gavin <iamdangavin@gmail.com>
2024-04-17 14:44:35 -0500

    Add Options

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # tailwindcss-hanging-puncuation
+
 Hanging punctuation support for TailwindCSS
+
+```
+hanging-none
+hanging-first
+hanging-last
+hanging-first-last
+hanging-first-force-end
+hanging-first-allow-end
+hanging-last-force-end
+hanging-last-allow-end
+hanging-first-force-end-last
+hanging-first-allow-end last
+hanging-allow-end
+hanging-force-end
+hanging-revert
+hanging-revert-layer
+hanging-unset
+hanging-inherit
+hanging-initial
+```
--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
-const hangingPunctuationPlugin = plugin(function({ addUtilities, theme, e }) {
+const hangingPunctuationPlugin = plugin(function ({ addUtilities, theme, e }) {
   const newUtilities = {};
-  const hangingOptions = theme('hangingPunctuation', {
-    none: 'none',
-    first: 'first',
-    last: 'last',
-    allowEnd: 'allow-end',
-    forceEnd: 'force-end'
+  const hangingOptions = theme("hangingPunctuation", {
+    none: "none",
+    first: "first",
+    last: "last",
+    "first-last": "first last",
+    "first-force-end": "first force-end",
+    "first-allow-end": "first allow-end",
+    "last-force-end": "last force-end",
+    "last-allow-end": "last allow-end",
+    "first-force-end-last": "first force-end last",
+    "first-allow-end last": "first allow-end last",
+    "allow-end": "allow-end",
+    "force-end": "force-end",
+    revert: "revert",
+    "revert-layer": "revert-layer",
+    unset: "unset",
+    inherit: "inherit",
+    initial: "initial",
   });
 
-  Object.keys(hangingOptions).forEach(key => {
+  Object.keys(hangingOptions).forEach((key) => {
     const value = hangingOptions[key];
     newUtilities[`.${e(`hanging-${key}`)}`] = { hangingPunctuation: value };
   });
 
-  addUtilities(newUtilities, ['responsive']);
+  addUtilities(newUtilities, ["responsive"]);
 });
 
 module.exports = hangingPunctuationPlugin;
